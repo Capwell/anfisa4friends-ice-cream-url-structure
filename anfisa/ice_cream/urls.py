513a4edf39832e1d029132fb43fd2b839bcaa1c9
Capwell <proninc@yandex.ru>
2021-11-07 19:13:33 +0400

    Update urls.py

--- anfisa/ice_cream/urls.py
+++ anfisa/ice_cream/urls.py
@@ -2,20 +2,16 @@ from django.urls import path
 
 from . import views
 
-# Эта строчка обязательна.
-# Без нее чуда не произойдет и namespace работать не будет
-app_name = 'ice_cream'
 
 urlpatterns = [
     # Главная страница
-    path('', views.index, name='index'),
+    path('', views.index),
     # Список мороженого
-    path('ice_cream/', views.ice_cream_list, name='icecream_list'),
+    path('ice_cream/', views.ice_cream_list),
     # Подробная информация о мороженом. Ждем пременную типа int,
     # и будем использовать ее под именем pk
     path(
         'ice_cream/<int:pk>/',
-        views.ice_cream_detail,
-        name='icecream_detail'
+        views.ice_cream_detail
     ),
 ]

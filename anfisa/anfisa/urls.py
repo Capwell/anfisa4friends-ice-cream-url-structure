cf0387e6844a0875f90d9d2c861c1249635a3ae8
Capwell <proninc@yandex.ru>
2021-11-07 19:07:56 +0400

    Update urls.py

--- anfisa/anfisa/urls.py
+++ anfisa/anfisa/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [    
     # Сначала проверяем все пути, которые есть в приложении ice_cream
-    path('', include('ice_cream.urls', namespace='ice_cream')),
+    path('', include('ice_cream.urls')),
     path('admin/', admin.site.urls),
 ]
